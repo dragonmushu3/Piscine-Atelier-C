d1a5a316fcdc445a8562b6715e49d7f9d3bab0cf
Mushu Damaschin <mushu.damaschin@epita.fr>
2021-09-27 18:59:36 +0200

    finished assignment pi gen

---
+++ pi_generator/pi_generator.c
@@ -0,0 +1,34 @@
+
+#include <stddef.h>
+
+
+
+
+
+
+
+
+
+
+
+
+double pi_generator(int precision)
+{
+    double pi_gen(int i)
+    {
+        if (i == precision)
+        {
+            double bab = i * 2 + 1;
+            double billy = i;
+            double bob = billy / bab;
+            return 1 + bob;
+        }
+        double bab = i * 2 + 1;
+        double billy = i;
+        double bob = billy / bab;
+
+        return 1 + bob * pi_gen(i+1);
+    }
+    return 2 * pi_gen(1);
+}
+
