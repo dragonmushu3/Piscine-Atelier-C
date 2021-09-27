7f6cc9491e509fd0fd5ee06259e8f23794019f1e
Mushu Damaschin <mushu.damaschin@epita.fr>
2021-09-27 20:53:56 +0200

    removed unnecessary print from my_pow

--- my_pow/my_pow.c
+++ my_pow/my_pow.c
@@ -8,7 +8,6 @@ int my_pow(int a, int b)
     int res = a;
     for(int i = 1; i <= (b / 2); i++)
     {
-        printf("%d iterations!\n",i);
         if ( i % 2 == 0)
         {
             res = res * res;
