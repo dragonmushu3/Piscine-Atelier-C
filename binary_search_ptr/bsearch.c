34f5ed2e48d2f0510faa0a02d66135188855a5a1
Mushu Damaschin <mushu.damaschin@epita.fr>
2021-09-28 18:55:26 +0200

    bsearch

---
+++ binary_search_ptr/bsearch.c
@@ -0,0 +1,35 @@
+#include <stddef.h>
+
+
+int *binary_search(int *begin, int *end, int elt)
+{
+    int diff = 0;
+    int inter = 0;
+    int *m = NULL;
+    int *rec(int *begin, int *end, int elt)
+    {
+        if (end - begin == 0)
+            return end;
+        else
+        {
+            inter = end - begin;
+            diff = inter / 2;
+            m = begin + diff;
+            if (elt == *m)
+                return m;
+            else if (elt > *m)
+            {
+                begin = m + 1;
+                return binary_search(begin,end,elt);
+            }
+            else 
+            {
+                end = m;
+                return binary_search(begin,end,elt);
+            }
+        }
+    }
+    return rec(begin, end, elt);
+}
+
+
