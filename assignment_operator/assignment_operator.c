0a214cc9ce558173fe90070540f228a65e950739
Mushu Damaschin <mushu.damaschin@epita.fr>
2021-09-28 22:54:50 +0200

    added null edge cases to assignment operator

--- assignment_operator/assignment_operator.c
+++ assignment_operator/assignment_operator.c
@@ -2,22 +2,28 @@
 
 void plus_equal(int *a, int*b)
 {
+    if ((!a) || (!b))
+        return;
     *a = *a + *b;
 }
 
 void minus_equal(int *a, int*b)
 {
+    if ((!a) || (!b))
+        return;
     *a = *a - *b;
 }
 
 void mult_equal(int *a, int *b)
 {
+    if ((!a) || (!b))
+        return;
     *a = *a * *b;
 }
 
 int div_equal(int *a, int*b)
 {
-    if ((!a) || (*b == 0))
+    if ((!a) || (!b) || (*b == 0))
         return 0;
     int res = (*a % *b);
     *a = *a / *b;
