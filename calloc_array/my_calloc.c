b36f97e6649d8d86e1392a6911bf4adde816ecf6
Mushu Damaschin <mushu.damaschin@epita.fr>
2021-09-28 22:41:41 +0200

    first verion of my_calloc, without overflow protection

---
+++ calloc_array/my_calloc.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+#include <stdlib.h>
+
+int *my_calloc(size_t size, int init)
+{
+    if (size == 0)
+        return NULL;
+    int *ptr = malloc(sizeof(int) * size);
+    if (!ptr)
+        return NULL;
+    for( size_t i = 0; i < size; i++)
+        *(ptr+i) = init;
+    return ptr;
+}
+
+
+
+
