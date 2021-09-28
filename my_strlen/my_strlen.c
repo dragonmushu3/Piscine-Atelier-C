38362a64e40ccde934fa4dbb7eeb838cd5d91c3d
Mushu Damaschin <mushu.damaschin@epita.fr>
2021-09-28 17:26:12 +0200

    done strlen

---
+++ my_strlen/my_strlen.c
@@ -0,0 +1,17 @@
+
+#include <stddef.h>
+
+
+size_t my_strlen(const char *s)
+{
+    if (!s)
+        return 0;
+    size_t i = 0;
+    while( *(s + i) != '\0')
+        i++;
+    return i;
+}
+
+
+
+
