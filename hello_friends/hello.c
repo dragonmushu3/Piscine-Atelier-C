d2335f69a94674bfbf556d9448cf8f8c80a71f39
Mushu Damaschin <mushu.damaschin@epita.fr>
2021-09-28 22:11:33 +0200

    submit hello_friends

---
+++ hello_friends/hello.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+
+int main(int argc, char **argv)
+{
+    if (argc == 1)
+        puts("Hello World!");
+    else
+    {
+        for(int i = 1; i < argc; i++)
+            printf("Hello %s!\n",argv[i]);
+    }
+    return 0;
+}
+
