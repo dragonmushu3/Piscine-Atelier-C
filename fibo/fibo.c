f48302211ce264a53a878594cf740ea154881cd5
Mushu Damaschin <mushu.damaschin@epita.fr>
2021-09-29 14:06:53 +0200

    clanged fibo

--- fibo/fibo.c
+++ fibo/fibo.c
@@ -1,13 +1,10 @@
 #include <stddef.h>
 
-
 unsigned long fibonacci(unsigned long n)
 {
     if (n == 0)
         return 0;
     if (n == 1)
         return 1;
-    return fibonacci(n-1) + fibonacci(n-2);
+    return fibonacci(n - 1) + fibonacci(n - 2);
 }
-
-
