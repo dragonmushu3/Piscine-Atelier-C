d8494f78df0f349f0423a9e261025c59e7f0d0d2
Mushu Damaschin <mushu.damaschin@epita.fr>
2021-09-29 14:13:11 +0200

    clanged calloc

--- calloc_array/my_calloc.c
+++ calloc_array/my_calloc.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-
-
 #include <stdlib.h>
 
 int *my_calloc(size_t size, int init)
@@ -10,11 +8,7 @@ int *my_calloc(size_t size, int init)
     int *ptr = malloc(sizeof(int) * size);
     if (!ptr)
         return NULL;
-    for( size_t i = 0; i < size; i++)
-        *(ptr+i) = init;
+    for (size_t i = 0; i < size; i++)
+        *(ptr + i) = init;
     return ptr;
 }
-
-
-
-
