58d13216e016aa08e56b6556f5f42065247a1d08
Mushu Damaschin <mushu.damaschin@epita.fr>
2021-09-29 14:23:42 +0200

    clanged str_revert

--- str_revert/str_revert.c
+++ str_revert/str_revert.c
@@ -1,19 +1,15 @@
 #include <stddef.h>
 
-
-
-
-
 void str_revert(char str[])
 {
     size_t i = 0;
-    while( *(str + i) != '\0')
+    while (*(str + i) != '\0')
         i++;
     char temp = 'a';
     for (size_t j = 0; j < i / 2; j++)
     {
         temp = str[j];
-        str[j] = str[i-j-1];
-        str[i-j-1] = temp;
+        str[j] = str[i - j - 1];
+        str[i - j - 1] = temp;
     }
 }
