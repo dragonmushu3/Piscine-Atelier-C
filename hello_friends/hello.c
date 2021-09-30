8d9eb70899ca8e177c321a333ddeddc9042442af
Mushu Damaschin <mushu.damaschin@epita.fr>
2021-09-30 14:22:25 +0200

    clanged hello_friends

--- hello_friends/hello.c
+++ hello_friends/hello.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 
-
-
 int main(int argc, char **argv)
 {
     if (argc == 1)
         puts("Hello World!");
     else
     {
-        for(int i = 1; i < argc; i++)
-            printf("Hello %s!\n",argv[i]);
+        for (int i = 1; i < argc; i++)
+            printf("Hello %s!\n", argv[i]);
     }
     return 0;
 }
-
