a98d7d913ca9e372b4032bb9f355ba22bc9e6d4f
Mushu Damaschin <mushu.damaschin@epita.fr>
2021-09-30 13:21:43 +0200

    clanged my_strlen again

--- my_strlen/my_strlen.c
+++ my_strlen/my_strlen.c
@@ -1,12 +1,11 @@
 #include <stddef.h>
 
-
 size_t my_strlen(const char *s)
 {
     if (!s)
         return 0;
     size_t i = 0;
-    while(*(s + i) != '\0')
+    while (*(s + i) != '\0')
         i++;
     return i;
 }
