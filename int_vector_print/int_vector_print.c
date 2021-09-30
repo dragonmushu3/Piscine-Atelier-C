d7dfe21112b358fd9238e33e836217c688e2255a
Mushu Damaschin <mushu.damaschin@epita.fr>
2021-09-30 13:48:24 +0200

    clanged vector print, removed putchar

--- int_vector_print/int_vector_print.c
+++ int_vector_print/int_vector_print.c
@@ -1,11 +1,11 @@
 #include "int_vector_print.h"
+
 #include <stddef.h>
 #include <stdio.h>
 
 void int_vector_print(const struct int_vector vec)
 {
-    for ( size_t i = 0; i < vec.size - 1; i++)
+    for (size_t i = 0; i < vec.size - 1; i++)
         printf("%d ", vec.data[i]);
-    printf("%d", vec.data[vec.size - 1]);
-    putchar('\n');
+    printf("%d\n", vec.data[vec.size - 1]);
 }
