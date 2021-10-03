104f9899fcccd0113323670cd4583df056aa48a7
Mushu Damaschin <mushu.damaschin@epita.fr>
2021-10-03 21:43:18 +0200

    fixed sieve to print n if it is a prime number

--- sieve_eratosthenes/sieve.c
+++ sieve_eratosthenes/sieve.c
@@ -3,7 +3,11 @@
 
 void sieve(int n)
 {
-    static int arr[1000] = {0};
+    static int arr[1000] =
+    {
+        0
+    };
+
     for (int i = 2; i < n; i++)
     {
         if (arr[i] == 0)
@@ -12,9 +16,9 @@ void sieve(int n)
                 arr[u] = 1;
         }
     }
-    for (int i = 2; i < n; i++)
+    for (int i = 2; i <= n; i++)
     {
         if (arr[i] == 0)
-            printf("%d\n",i);
+            printf("%d\n", i);
     }
 }
