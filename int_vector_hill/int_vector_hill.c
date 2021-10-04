6875739613b61ed54f3c6a54da8e9eef5ad084dc
Mushu Damaschin <mushu.damaschin@epita.fr>
2021-10-04 16:50:41 +0200

    fixed negative hill case

--- int_vector_hill/int_vector_hill.c
+++ int_vector_hill/int_vector_hill.c
@@ -6,8 +6,13 @@ int int_vector_hill(struct int_vector vec)
 {
     size_t i = 1;
     int res = 0;
-    if (vec.size == 1)
-        return 0;
+    if (vec.size == 1 && vec.data[0] >= 0)
+        return vec.data[0];
+    for (size_t i = 0; i < vec.size; i++)
+    {
+        if (vec.data[i] < 0)
+            return -1;
+    }
     while (i < vec.size && vec.data[i] >= vec.data[i - 1])
     {
         if (vec.data[i] > vec.data[i - 1])
@@ -23,4 +28,9 @@ int int_vector_hill(struct int_vector vec)
     if (!(bob == billy))
         return -1;
     return res;
+}
+
+int main(void)
+{
+    return 0;
 }
