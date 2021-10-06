4edebc7739da880e89b60ca718d933d45294678d
Mushu Damaschin <mushu.damaschin@epita.fr>
2021-10-06 17:57:41 +0200

    submitting my_strcpy

---
+++ my_strcpy/my_strcpy.c
@@ -0,0 +1,13 @@
+#include <stddef.h>
+
+char *my_strcpy(char *dest, const char *source)
+{
+    size_t i = 0;
+    while (s[i] != '\0')
+    {
+        dest[i] = source[i];
+        i++;
+    }
+    dest[i + 1] = '\0';
+    return res;
+}
