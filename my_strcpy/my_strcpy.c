6f96f27539976e85184dd2dea675d0ce82afd6f6
Mushu Damaschin <mushu.damaschin@epita.fr>
2021-10-07 13:49:07 +0200

    removed undeclared res and s from my_strcpy

--- my_strcpy/my_strcpy.c
+++ my_strcpy/my_strcpy.c
@@ -3,11 +3,11 @@
 char *my_strcpy(char *dest, const char *source)
 {
     size_t i = 0;
-    while (s[i] != '\0')
+    while (source[i] != '\0')
     {
         dest[i] = source[i];
         i++;
     }
     dest[i + 1] = '\0';
-    return res;
+    return dest;
 }
