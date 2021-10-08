f7acdaa9f452a5314c4cf9cf5e40ef3d0c39225a
Mushu Damaschin <mushu.damaschin@epita.fr>
2021-10-08 15:57:17 +0200

    nothing

--- bst/main.c
+++ bst/main.c
@@ -9,10 +9,11 @@ int main(void)
 {
     struct bst *tree = init( 10);
 
-    add(tree, 5);
-    add(tree, 2);
-    add(tree, 6);
-    add(tree, 80);
+    for (size_t i = 10; i < 20; i++)
+    {
+        add(tree, i);
+        add(tree, i - 1);
+    }
 
     for (size_t i = 0; i < tree->capacity; i++)
     {
