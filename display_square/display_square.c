cb5223dc77c0eef93c4c83e2311cd54d52b0bc80
Mushu Damaschin <mushu.damaschin@epita.fr>
2021-10-11 16:27:24 +0200

    submit display_square

---
+++ display_square/display_square.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void display_square(int width)
+{
+    if (width <= 0)
+        return;
+    if (width % 2 == 0)
+        width++;
+    int rows = (width + 1) / 2;
+    for (int i = 0; i < width; i++)
+        putchar('*');
+    putchar('\n');
+    for (int i = 0; i < rows - 2; i++)
+    {
+        putchar('*');
+        for (int j = 0; j < width - 2; j++)
+        {
+            putchar(32);
+        }
+        putchar('*');
+        putchar('\n');
+    }
+    for (int i = 0; i < width; i++)
+        putchar('*');
+    putchar('\n');
+}
