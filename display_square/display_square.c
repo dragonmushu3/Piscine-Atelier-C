9895be02f16b647c261150e9f5c32303788db712
Mushu Damaschin <mushu.damaschin@epita.fr>
2021-10-13 18:46:52 +0200

    fixed width = 1

--- display_square/display_square.c
+++ display_square/display_square.c
@@ -6,6 +6,12 @@ void display_square(int width)
         return;
     if (width % 2 == 0)
         width++;
+    if (width == 1)
+    {
+        putchar('*');
+        putchar('\n');
+        return;
+    }
     int rows = (width + 1) / 2;
     for (int i = 0; i < width; i++)
         putchar('*');
