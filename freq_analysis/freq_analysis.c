a98a0ba4d975efc1bd9a7b2228b9ba05fc9c839c
Mushu Damaschin <mushu.damaschin@epita.fr>
2021-11-08 14:17:18 +0100

    freq_analysis ready

--- freq_analysis/freq_analysis.c
+++ freq_analysis/freq_analysis.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+struct info
+{
+	const char *text;
+	const char *table;
+	size_t unique_chars;
+};
+
 size_t my_strlen(const char *str)
 {
 	size_t i = 0;
@@ -44,24 +51,45 @@ size_t count(const char text[], char to_count)
 	return counter;
 }
 
+int find_corresponding(struct info inf, size_t freq, size_t limit)
+{
+	size_t max_freq = 0;
+	int curr_char = 0;
+	for (size_t i = 0; i < inf.unique_chars; i++)
+	{
+		curr_char = next_alpha(inf.text, curr_char);
+		size_t temp = count(inf.text, curr_char);
+		if (temp > max_freq && temp < limit)
+			max_freq = temp;
+	}
+	if (max_freq == freq)
+		return *inf.table;
+	else
+	{
+		inf.table++;
+		return find_corresponding(inf, freq, max_freq);
+	}	
+}
+
 void freq_analysis(const char text[], const char table[])
 {
 	int curr_char = 0;
+	int unique_char = table[0];
 	size_t unique_chars = my_strlen(table);
+
+	struct info inf;
+	inf.text = text;
+	inf.table = table;
+	inf.unique_chars = unique_chars;
+
 	for (size_t i = 1; i <= unique_chars; i++)
 	{
 		size_t freq = 0;
 		curr_char = next_alpha(text, curr_char);
 		freq = count(text, curr_char);
-		printf("%c has %ld occurences\n", curr_char, freq);
+
+		unique_char = find_corresponding(inf, freq, 91);
+
+		printf("%c %c\n", curr_char, unique_char);
 	}
-
-}
-
-int main(int argc, char *argv[])
-{
-	if (argc != 3)
-		return 1;
-	freq_analysis(argv[1], argv[2]);
-	return 0;
 }
