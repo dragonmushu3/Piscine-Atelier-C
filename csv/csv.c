a1f48ab16a3ffe43782f46ffbc8a1fa334153978
Mushu Damaschin <mushu.damaschin@epita.fr>
2021-11-10 02:34:14 +0100

    submission for csv ready but wait for clang

---
+++ csv/csv.c
@@ -0,0 +1,45 @@
+#define _DEFAULT_SOURCE
+#include <stdio.h>
+#include <err.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc == 1)
+        return 1;
+
+    FILE *csv = fopen(argv[1], "r");
+    if (!csv)
+        err(1, "%s", argv[1]);
+    
+    size_t n = 0;
+
+    char *line_ptr = NULL;
+    char *token = NULL;
+    while (getline(&line_ptr, &n, csv) != -1)
+    {
+        char *saveptr;
+        token = strtok_r(line_ptr, ",", &saveptr);
+        if (!token)
+        {
+            printf("%d", 0);
+            continue;
+        }
+        
+        int max = atoi(token);
+        while (token)
+        {
+            int token_value = atoi(token);
+            if (token_value > max)
+                max = token_value;
+            token = strtok_r(NULL, ",", &saveptr);
+        }
+        printf("%d\n", max);
+    }
+
+    free(token);
+    free(line_ptr);
+    fclose(csv);
+    return 0;
+}
