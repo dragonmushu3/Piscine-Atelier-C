821791164e8bf46482f6936066a10439d6d36ec9
Mushu Damaschin <mushu.damaschin@epita.fr>
2021-11-12 08:39:53 +0100

    tools to more easily work on insert_line

---
+++ file_basics_insert_line/insert_line.h
@@ -0,0 +1,10 @@
+#ifndef INSERT_LINE_H
+#define INSERT_LINE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int insert_line(const char *file_in, const char *file_out, const char *content,
+                size_t n);
+
+#endif /* !INSERT_LINE_H */
