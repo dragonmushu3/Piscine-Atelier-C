27affb33266a78abc77b9c943ee213bc893fe9e2
Mushu Damaschin <mushu.damaschin@epita.fr>
2021-11-14 17:03:40 +0100

    rewrote binary_search

--- binary_search/binary_search.c
+++ binary_search/binary_search.c
@@ -3,22 +3,24 @@
 
 int binary_search(const int vec[], size_t size, int elt)
 {
+    if (size == 0)
+        return -1;
+
     size_t min = 0;
     size_t max = size - 1;
-    size_t m = (max - min) / 2;
     while (!(max <= min))
     {
+        size_t m = (max - min) / 2;
         if (elt == vec[m])
             return m;
-        if (elt > vec[m + min])
+        else if (elt > vec[m])
         {
-            min = m + 1;
+            binary_search(m + 1, size - m, elt);
         }
         else
         {
-            max = m - 1;
+            binary_search(vec, size - m, elt);
         }
-        m = (max + min) / 2;
     }
     return -1;
 }
