28ab6c756515acd5817070dda89a6dccd7b3635b
Mushu Damaschin <mushu.damaschin@epita.fr>
2021-11-14 19:27:20 +0100

    fix

--- bubble_sort/bubble_sort.c
+++ bubble_sort/bubble_sort.c
@@ -3,15 +3,15 @@
 void bubble_sort(int array[], size_t size)
 {
     int temp = 0;
-    for (size_t j = 1; j < size; j++)
+    for (size_t j = 0; j < size; j++)
     {
-        for (size_t i = 0; i < size; i++)
+        for (size_t i = 0; i < size - 1; i++)
         {
-            if (array[i] < array[i - 1])
+            if (array[i] > array[i + 1])
             {
-                temp = array[i - 1];
-                array[i - 1] = array[i];
-                array[i] = temp;
+                temp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
             }
         }
     }
