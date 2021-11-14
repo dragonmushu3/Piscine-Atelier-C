511fd749ca5d2bfec4522ee883eed63acbcae144
Mushu Damaschin <mushu.damaschin@epita.fr>
2021-11-14 23:04:07 +0100

    fixed fibo sequence underfined initialiser

--- fibo_iter/fibo_iter.c
+++ fibo_iter/fibo_iter.c
@@ -4,7 +4,7 @@ unsigned long fibo_iter(unsigned long n)
 {
     unsigned long u0 = 0;
     unsigned long u1 = 1;
-    unsigned long res;
+    unsigned long res = 0;
     
     for (unsigned long i = 0; i < n; i++)
     {
