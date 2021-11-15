df46e51cdfba4d9034d8384f2b98bf1f44f775af
Mushu Damaschin <mushu.damaschin@epita.fr>
2021-11-15 17:44:51 +0100

    trying to fix atoi

--- my_atoi/my_atoi.c
+++ my_atoi/my_atoi.c
@@ -11,13 +11,40 @@ size_t my_strlen(const char *s)
     return res;
 }
 
+
 int my_atoi(const char *str)
 {
+    while (*str == 32)
+    {
+        str++;
+    }
+    
+    int is_negative = 0;
+    if (*str == '\0')
+    {
+        return 0;
+    }
+
+    if (*str == '-')
+    {
+        is_negative = 1;
+        str++;
+    }
+    else if (*str == '+')
+    {
+        str++;
+    }
+
+    if (*str > 57 || *str < 48)
+        return 0;
+    
     size_t digits = my_strlen(str);
     int res = 0;
 
     for (int i = digits - 1; i >= 0; i--)
     {
+        if (str[i] > 57 || str[i] < 48)
+            return 0;
         if (digits - 1 - i == 0)
             res += str[i] - 48;
         else
@@ -30,5 +57,15 @@ int my_atoi(const char *str)
             res += (str[i] - 48) * ten_pow;
         }
     }
-    return res;
+    
+
+    if (is_negative)
+        return -res;
+    else
+        return res;
+}
+
+int main(void)
+{
+    return my_atoi("  -156");
 }
