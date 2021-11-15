5e216eec97c76ff50c05556a34f30cd48ea92911
Mushu Damaschin <mushu.damaschin@epita.fr>
2021-11-15 17:14:22 +0100

    maybe fixed append file

--- file_basics_append_file/append_file.c
+++ file_basics_append_file/append_file.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <err.h>
 
 int append_file(const char *file_in, const char *content)
 {
     if (!file_in || !content)
         return -1;
 
-    FILE *f_in = fopen(file_in, "w");
+    FILE *f_in = fopen(file_in, "a");
 
     if (!f_in)
+        return -1;
+
+    if (fseek(f_in, 0, SEEK_END) == -1)
         return -1;
 
     fprintf(f_in, "%s", content);
