9c25749099fe5a2a5c05324fbefecad958d42108
Mushu Damaschin <mushu.damaschin@epita.fr>
2021-11-15 02:12:55 +0100

    fix

--- vmax_ptr/vmax_ptr.c
+++ vmax_ptr/vmax_ptr.c
@@ -3,7 +3,7 @@
 int vmax(const int *ptr, size_t size)
 {
     int max = *ptr;
-    int vice_max = *ptr;
+    int vice_max = 0;
 
     for (size_t i = 0; i < size; i++)
     {
@@ -12,6 +12,10 @@ int vmax(const int *ptr, size_t size)
             int temp = max;
             max = ptr[i];
             vice_max = temp;
+        }
+        else if (ptr[i] > vice_max && ptr[i] < max)
+        {
+            vice_max = ptr[i];
         }
     }
     return vice_max;
