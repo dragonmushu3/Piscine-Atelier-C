59c113255eee513eab9ac9eb266ac0b7507c1787
Mushu Damaschin <mushu.damaschin@epita.fr>
2021-11-16 18:59:54 +0100

    testing facilities for freq_analysis

---
+++ freq_analysis/freq_analysis.h
@@ -0,0 +1,8 @@
+#ifndef FREQ_ANALYSIS_H
+#define FREQ_ANALYSIS_H
+
+#include <stdio.h>
+
+void freq_analysis(const char text[], const char table[]);
+
+#endif /* !FREQ_ANALYSIS_H */
---
+++ freq_analysis/main.c
@@ -0,0 +1,9 @@
+#include "freq_analysis.h"
+
+int main(void)
+{
+    const char text[] = "GJJKKKRRSSSS";
+    const char table[] = "GJKRS";
+    freq_analysis(text, table);
+    return 0;
+}
