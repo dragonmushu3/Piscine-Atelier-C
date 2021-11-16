7c839f2932efb066543e4c84e4206d22f8de8f76
Mushu Damaschin <mushu.damaschin@epita.fr>
2021-11-16 15:17:16 +0100

    added testing utilities for heap_sort

---
+++ heap_sort/heap_sort.h
@@ -0,0 +1,10 @@
+#ifndef HEAP_SORT_H
+#define HEAP_SORT_H
+
+#include <stddef.h>
+
+void heapify(int *array, size_t size);
+
+void heap_sort(int *array, size_t size);
+
+#endif
---
+++ heap_sort/test_main.c
@@ -0,0 +1,85 @@
+#include "heap_sort.h"
+#include <time.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+
+void Test_1(void)
+{
+    srand(time(NULL));
+    int correct = 1;
+
+
+    int passed_tests = 0;
+    while (correct)
+    {
+        int size = rand();
+        if (size > 100)
+            size = 100;
+        int *arr = malloc(sizeof(int) * size);
+        for (int i = 0; i < size; i++)
+        {
+            arr[i] = rand();
+        }
+        heap_sort(arr, size);
+
+        for (int i = 0; i < size - 1; i++)
+        {
+            if (arr[i] > arr[i + 1])
+            {
+                correct = 0;
+                break;
+            }
+        }
+
+        if (!correct)
+        {
+            printf("Found bad sort. Check bad_sort file.\n");
+            FILE *out = fopen("bad_sort", "w+");
+            for (int i = 0; i < size; i++)
+            {
+                fprintf(out, "%d ", arr[i]);
+            }
+            fprintf(out, "\n Test %d failed.\n", passed_tests + 1);
+            free(arr);
+            fclose(out);
+        }
+        else
+        {
+            //printf("#############################################\n");
+            //for (int i = 0; i < size; i++)
+            //{
+            //    printf("%d ", arr[i]);
+            //}
+            //printf("\n\n");
+            passed_tests++;
+            //printf("Passed %d tests.\n\n", passed_tests);
+            //printf("#############################################\n");
+            free(arr);
+        }
+    }
+}
+
+void Test_2(void)
+{
+    int arr[1] = {5};
+    heapify(arr, 1);
+    for (int i = 0; i < 1; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    heap_sort(arr, 1);
+    puts("");
+    for (int i = 0; i < 1; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+}
+
+int main(void)
+{
+    //Test_1();
+    Test_2();
+    return 0;
+}
