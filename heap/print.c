72d216ea2531d77d9e9013dd111c2227bd502815
Mushu Damaschin <mushu.damaschin@epita.fr>
2021-11-16 10:18:27 +0100

    fixed print_heap, but why was there fsanitize in the trace?

--- heap/print.c
+++ heap/print.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 #include "heap.h"
 
 unsigned int digit(int l, int k)
@@ -17,28 +18,38 @@ unsigned int digit(int l, int k)
     return l % quotient;
 }
 
+int count_digits(int n)
+{
+    if (n < 0)
+        n = -n;
+    if (n == 0)
+        return 1;
+    
+    int no_digits = 0;
+    while (!(n == 0))
+    {
+        n = n / 10;
+        no_digits++;
+    }
+    return no_digits;
+}
+
 void print_heap(const struct heap *heap)
 {
     for (size_t arr_index = 0; arr_index < heap->size; arr_index++)
     {
         /*count element digits*/
+        int no_digits = count_digits(heap->array[arr_index]);
 
-        int temp = heap->array[arr_index];
-        if (temp < 0)
-            temp = -temp;
-        int no_digits = 0;
-
-        while (!(temp == 0))
-        {
-            temp = temp / 10;
-            no_digits++;
-        }
         /*print each digit*/
         int to_print = heap->array[arr_index];
         if (to_print < 0)
+        {
             to_print = -to_print;
+            putchar('-');
+        }
 
-        for (int i = 1; i <= no_digits; i++)
+        for (int i = no_digits; i >= 1; i--)
         {
             putchar(digit(to_print, i) + 48);
         }
