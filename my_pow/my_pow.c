91fa3b766a6f5fa70155cdccd25e502f1fdcb536
Mushu Damaschin <mushu.damaschin@epita.fr>
2021-11-18 00:06:11 +0100

    mypow

--- my_pow/my_pow.c
+++ my_pow/my_pow.c
@@ -3,14 +3,15 @@
 
 int my_pow(int a, int b)
 {
-    int res = a;
-    for (int i = 1; i <= (b / 2); i++)
+    if (b == 0)
+        return 1;
+
+    int old_a = a;
+    int ref_a = a;
+    for (size_t i = 0;, i < b - 1; i++)
     {
-        if (i % 2 == 0)
-        {
-            res = res * res;
-        }
-        res = res * a;
+        old_a = a;
+        a = ref_a * old_a;
     }
-    return res;
+    return a;
 }
