c07c65367e61792b721dd389d01e1a82fd5744ce
Mushu Damaschin <mushu.damaschin@epita.fr>
2021-11-18 01:34:18 +0100

    sub my_itoa

---
+++ my_itoa/my_itoa.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned int digit(int l, int k)
+{
+    if ((l <= 0) || (k <= 0))
+        return 0;
+    unsigned int quotient = 1;
+    for (int i = 1; i <= k; i++)
+        quotient *= 10;
+    if (k >= 2)
+    {
+        unsigned int number1 = l % quotient;
+        unsigned int number2 = l % (quotient / 10);
+        return (number1 - number2) / (quotient / 10);
+    }
+    return l % quotient;
+}
+
+int count_digits(int n)
+{
+    if (n < 0)
+        n = -n;
+    if (n == 0)
+        return 1;
+
+    int no_digits = 0;
+    while (!(n == 0))
+    {
+        n = n / 10;
+        no_digits++;
+    }
+    return no_digits;
+}
+
+char *my_itoa(int value, char *s)
+{
+    char *res = s;
+
+    if (value < 0)
+    {
+        value = -value;
+        s[0] = '-';
+        s++;
+    }
+
+    int no_digits = count_digits(value);
+
+    for (int i = 0; i < no_digits; i++)
+    {
+        s[i] = digit(value, no_digits - i) + 48;
+    }
+
+    return res;
+}
+
+int main(void)
+{
+    int ex = -546;
+
+    char *number = malloc(5);
+    number[4] = '\0';
+    number = my_itoa(ex, number);
+    printf("%s\n", number);
+    free(number);
+    return 0;
+}
