5ca0fd56ed608d04b2883764d0992eb586500b29
Mushu Damaschin <mushu.damaschin@epita.fr>
2021-11-18 02:07:02 +0100

    itoa testing

---
+++ my_itoa/main.c
@@ -0,0 +1,32 @@
+#include "my_itoa.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+
+int main(void)
+{
+    char *number = malloc(2);
+
+    printf("%s\n", my_itoa(0, number));
+    assert(strlen(number) == 1);
+    free(number);
+
+    char *number2 = malloc(3);
+
+    printf("%s\n", my_itoa(25, number2));
+    assert(strlen(number2) == 2);
+    free(number2);
+
+    char *number3 = malloc(4);
+
+    printf("%s\n", my_itoa(258, number3));
+    assert(strlen(number3) == 3);
+    free(number3);
+
+    char *number4 = malloc(5);
+
+    printf("%s\n", my_itoa(-462, number4));
+    assert(strlen(number4) == 4);
+    free(number4);
+}
