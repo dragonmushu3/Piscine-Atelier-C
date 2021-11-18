bf93647dcd2b7e7d2b5b68fd3f6415500b3c2ac7
Mushu Damaschin <mushu.damaschin@epita.fr>
2021-11-18 01:00:07 +0100

    rewrote rot_x

--- ascii_carousel/rot_x.c
+++ ascii_carousel/rot_x.c
@@ -1,23 +1,90 @@
 #include <stddef.h>
 #include <stdio.h>
 
+int get_right_letter(char c, int x)
+{
+    if ('A' <= c && c <= 'Z')
+    {
+        if (x < 0)
+        {
+            x = -x;
+            int i = 0;
+            while (i < x)
+            {
+                if (c == 'A')
+                {
+                    c = 'Z';
+                    i++;
+                    continue;
+                }
+                c = c - 1;
+                i++;
+            }
+            return c;
+        }
+        else
+        {
+            int i = 0;
+            while (i < x)
+            {
+                if (c == 'Z')
+                {
+                    c = 'A';
+                    i++;
+                    continue;
+                }
+                c = c + 1;
+                i++;
+            }
+            return c;
+        }
+    }
+    else if ('a' <= c && c <= 'z')
+    {
+        if (x < 0)
+        {
+            x = -x;
+            int i = 0;
+            while (i < x)
+            {
+                if (c == 'a')
+                {
+                    c = 'z';
+                    i++;
+                    continue;
+                }
+                c = c - 1;
+                i++;
+            }
+            return c;
+        }
+        else
+        {
+            int i = 0;
+            while (i < x)
+            {
+                if (c == 'z')
+                {
+                    c = 'a';
+                    i++;
+                    continue;
+                }
+                c = c + 1;
+                i++;
+            }
+            return c;
+        }
+    }
+    return c;
+}
+
 void rot_x(char *s, int x)
 {
     for (size_t i = 0; s[i] != '\0'; i++)
     {
         if (('A' <= s[i] && s[i] <= 'Z') || ('a' <= s[i] && s[i] <= 'z'))
         {
-            printf("%c\n", s[i] + x);
-            s[i] = s[i] + x;
+            s[i] = get_right_letter(s[i], x);
         }
     }
-}
-
-int main()
-{
-    char in[] = "ShhhShEisZZZzZ059%";
-    printf("In: %s\n", in);
-    rot_x(in, 13);
-    printf("Out: %s\n", in);
-    return 0;
 }
